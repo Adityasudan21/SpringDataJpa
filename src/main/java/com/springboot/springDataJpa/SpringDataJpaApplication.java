4cdb855f84ce4943d8ee597ca45a33e7798c43f1
Adityasudan21 <adityasudan21@gmail.com>
2026-03-04 03:11:00 +0530

    JPQL and @Query Annotation is added and CRUD operations are added, DSL JPA queries are added in Spring Data JPA

--- src/main/java/com/springboot/springDataJpa/SpringDataJpaApplication.java
+++ src/main/java/com/springboot/springDataJpa/SpringDataJpaApplication.java
@@ -31,9 +31,19 @@ public class SpringDataJpaApplication {
 		s3.setName("Anushka");
 		s3.setMarks(110);
 
-		repo.save(s1);
-		repo.save(s2);
-		repo.save(s3);
+		repo.save(s1); //Create table if not there then save data in table
+//		repo.save(s2); // This also updates the value if same primary key is found
+//		repo.save(s3);
+		System.out.println(repo.findAll()); // Fetch all records from the table
+		System.out.println(repo.findById(2).orElse(new Student())); // Fetch record By ID and return null class if not found
+
+		System.out.println(repo.findByMarks(110)); //Custom Query
+
+		System.out.println(repo.findByMarksOrNameContaining(1001,"Adi")); //Custom Query
+
+		System.out.println(repo.findByNames("Adit","Anukriti")); // Custom Query using @Query
+
+//		repo.delete(s1); //This delete the record by Primary Key
 	}
 
 }
--- src/main/java/com/springboot/springDataJpa/model/StudentRepo.java
+++ src/main/java/com/springboot/springDataJpa/model/StudentRepo.java
@@ -1,7 +1,10 @@
 package com.springboot.springDataJpa.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student,Integer> {
@@ -9,4 +12,16 @@ public interface StudentRepo extends JpaRepository<Student,Integer> {
     // First one is the class name second one the Datatype of Primary Key (rollno in this case)
 
     //This gives all the functions that we need like CRUD operations
+
+    // Custom Queries
+
+    public List<Student> findByMarks(int marks);  // DSL will generate the custom query just based on the name
+
+    // For complex queries also we could do this by stacking up keyword one after another
+
+    public List<Student> findByMarksOrNameContaining(int marks,String name);
+
+    // Using @Query annotation to write veru complex query
+    @Query("select s from Student s where s.name=?1 OR s.name=?2")
+    public List<Student> findByNames(String name1, String name2);
 }
